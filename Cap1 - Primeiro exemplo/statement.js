d50ecbd525f79578f521aad885c7c1945330d717
Carlos Lima <carloslima@pactosolucoes.com.br>
2020-11-15 17:05:39 -0300

    refactor: move to extenal function switch code

--- Cap1 - Primeiro exemplo/statement.js
+++ Cap1 - Primeiro exemplo/statement.js
@@ -4,29 +4,11 @@ function statement(invoice, plays){
   let result = `Statement for ${invoice.customer}\n`;
   const format = new Intl.NumberFormat("en-US",
     { style: "currency", currency: "USD",
-      minimumFractionDigits: 2}).format;
+      minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20){
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     // Soma créditos por volume
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -41,7 +23,31 @@ function statement(invoice, plays){
 
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+
   return result;
+}
+
+function amountFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20){
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+
+  return thisAmount;
 }
 
 export default statement
