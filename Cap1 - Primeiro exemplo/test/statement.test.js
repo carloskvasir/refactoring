9a374d2c003e25864180b126c308b1d97835c7c7
Carlos Lima <carloslima@pactosolucoes.com.br>
2020-11-17 00:24:23 -0300

    test: case error with data

--- Cap1 - Primeiro exemplo/test/statement.test.js
+++ Cap1 - Primeiro exemplo/test/statement.test.js
@@ -2,10 +2,22 @@ import statement from '../statement.js';
 
 import fs from 'fs';
 import path from 'path';
+describe('Capitulo 1', () => {
+  test('example statement ok', () => {
+    const invoice = JSON.parse(fs.readFileSync(path.resolve(__dirname, "invoice.json")));
+    const plays = JSON.parse(fs.readFileSync(path.resolve(__dirname, "plays-ok.json")));
+    const statement_string = statement(invoice, plays);
+    expect(statement_string).toMatchSnapshot();
+  });
 
-test('example statement', () => {
-  const invoice = JSON.parse(fs.readFileSync(path.resolve(__dirname, "invoice.json")));
-  const plays = JSON.parse(fs.readFileSync(path.resolve(__dirname, "plays.json")));
-  const statement_string = statement(invoice, plays);
-  expect(statement_string).toMatchSnapshot();
+  test('example statement fails', () => {
+    const invoice = JSON.parse(fs.readFileSync(path.resolve(__dirname, "invoice.json")));
+    const plays = JSON.parse(fs.readFileSync(path.resolve(__dirname, "plays-error.json")));
+
+    function invalid(){
+      statement(invoice, plays);
+    }
+
+    expect(invalid).toThrowError('unknown type');
+  });
 });
